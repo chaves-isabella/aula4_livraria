b96966b57b279a21ef7ced6adf8b9bd343833798
chaves-isabella <isaachaves9@gmail.com>
2026-08-05 15:52:16 -0300

    feat:livaria

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,5 @@ void main() {
     Vendedor novoFuncionario = new Vendedor("Maria", "111.111.111-11", "(11)99999-9999", "maria@gmail.com",
             "vendedor_id: 101");
     Cliente novoCliente = new Cliente("João", "222.222.222-22", "(11)98888-8888",
-            " R. Bento Branco de Andrade Filho, 379 - Santo Amaro, São Paulo");
+            " R.  Bento Branco de Andrade Filho, 379 - Santo Amaro, São Paulo");
 ;}
